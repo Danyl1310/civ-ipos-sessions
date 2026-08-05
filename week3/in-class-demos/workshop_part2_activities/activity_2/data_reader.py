4d69df5f23d1f28140ce544416a6d238f74bca3b
20126596 <20126596@tafe.wa.edu.au>
2026-08-05 15:25:56 +0800

    feat: Week3 workshop P2 Act2 completed

--- week3/in-class-demos/workshop_part2_activities/activity_2/data_reader.py
+++ week3/in-class-demos/workshop_part2_activities/activity_2/data_reader.py
@@ -24,19 +24,45 @@ product_revenue = {}
 for sale in sales_data:
     product = sale['Product']
     revenue = sale['Total Revenue']
-    product_revenue[product] = product_revenue.get(product, 0)
+    product_revenue[product] = product_revenue.get(product, revenue)
+    product_revenue = dict(sorted(product_revenue.items(), key=lambda item: item[1], reverse=True))
     
 # TODO: Identify the product with the highest total units sold
+max_units_sold_product = {'Product' : 0}
+for sale in sales_data:
+    product = sale['Product']
+    sale_units = sale['Units Sold']
+    if sale_units > list(max_units_sold_product.values())[0]:
+        max_units_sold_product = {product : sale_units}
+    elif sale_units == list(max_units_sold_product.values())[0]:
+        max_units_sold_product[product] = sale_units
 
 # TODO: Calculate average unit price for each product - watch out for division by zero
+product_unit_price = {}
+# sort the product data into a dictionary
+for sale in sales_data:
+    product = sale['Product']
+    unit_price = sale['Unit Price']
+    if product not in product_unit_price:
+        product_unit_price[product] = [float(unit_price)]
+    elif product in product_unit_price:
+        product_unit_price[product].append(float(unit_price))
+print(product_unit_price)
+# Average the values
+for product in product_unit_price:
+    num_of_values = len(product_unit_price[product])
+    sum_of_values = sum(product_unit_price[product])
+    average_price = sum_of_values / num_of_values
+    product_unit_price[product] = average_price
 
 # Display results
 print("Total revenue for each product:")
 for product, revenue in product_revenue.items():
     print(f"{product}: ${revenue:.2f}")
 
-print("\nThe product with the highest total units sold:")
-print(max_units_sold_product)
+print("\nThe product/s with the highest total units sold:")
+for key, value in max_units_sold_product.items():
+    print(f"{key}: {value}")
 
 print("\nAverage unit price for each product:")
 for product, avg_price in product_unit_price.items():
